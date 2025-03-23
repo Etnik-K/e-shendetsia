fdb1e7f899f40a13163e79a383f142aa83b42abb
etnik <etnikkelmendi20@gmail.com>
2025-03-23 22:43:22 +0100

    Added the classes: Doctor and Clinic

---
+++ src/main/java/Controller/ClinicController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import Model.Clinic;
+import Model.Doctor;
+import Service.ClinicService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/clinics")
+public class ClinicController {
+    private ClinicService clinicService;
+
+    @GetMapping
+    public List<Clinic> getAllDoctors(){
+        return clinicService.getAllClinics();
+    }
+
+    @GetMapping("/{cid}")
+    public ResponseEntity<Clinic> getDoctorById(@PathVariable("cid") Long cid){
+        Optional<Clinic> clinic = clinicService.getClinicById(cid);
+        return clinic.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+}
