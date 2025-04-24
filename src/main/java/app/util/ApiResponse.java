2e9e09642ffdf345257511cae418be4f4e0bb2b4
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-04-24 23:30:08 +0200

    ristrukturimi i plote

--- src/main/java/app/util/ApiResponse.java
+++ src/main/java/app/util/ApiResponse.java
@@ -1,4 +1,14 @@
 package app.util;
 
-public class ApiResponse {
+
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String error;
+
+    public ApiResponse(boolean success, T data, String error) {
+        this.success = success;
+        this.data = data;
+        this.error = error;
+    }
 }
