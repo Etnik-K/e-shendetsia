7be483644e6d557dc012240ed50fe48b95d6392e
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-05-07 21:45:54 +0200

    test endpoint

--- src/main/java/app/util/BaseController.java
+++ src/main/java/app/util/BaseController.java
@@ -3,6 +3,7 @@ package app.util;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @ResponseBody
 @RequestMapping("/api")
-public abstract class BaseController {
+public class BaseController {
 
     protected <T> ResponseEntity<ApiResponse<T>> ok(T data) {
         return ResponseEntity.ok(new ApiResponse<>(true, data, null));
@@ -19,6 +20,11 @@ public abstract class BaseController {
     protected <T> ResponseEntity<ApiResponse<T>> error(String errorMessage) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ApiResponse<>(false, null, errorMessage));
+    }
+
+    @GetMapping("/test")
+    public String test(){
+    	return "Pershendetje nga Backend";
     }
 
 }
