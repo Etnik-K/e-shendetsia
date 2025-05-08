0595cd2e356d292abfeb1d03ac975bed87d47910
endrit <endritkastrati8@gmail.com>
2025-05-09 01:09:02 +0200

    Document main files with implementation mostly complete

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,6 +3,10 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Klasa kryesore qe fillon aplikacionin SpringBoot.
+ * Pergjegjes per inicializimin e kontestev te SpringBootit.
+ */
 @SpringBootApplication(scanBasePackages = "app")
 public class Main {
 
--- src/main/java/app/util/ApiResponse.java
+++ src/main/java/app/util/ApiResponse.java
@@ -3,6 +3,12 @@ package app.util;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Një klasë gjenerike që përfaqëson përgjigjen e API-t.
+ * Përdoret për të standardizuar formatin e përgjigjeve në të gjithë aplikacionin.
+ *
+ * @param <T> Lloji i të dhënave që do të kthehet
+ */
 @Getter
 @AllArgsConstructor
 public class ApiResponse<T> {
