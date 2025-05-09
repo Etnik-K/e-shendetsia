107227577ba73e9e6d16abba10a4ef4e3beeaaf1
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-05-09 02:38:14 +0200

    update doctorController
    rregullimi i nje errori kaskadik ne doctor controller

--- src/main/java/app/model/doctor/DoctorController.java
+++ src/main/java/app/model/doctor/DoctorController.java
@@ -4,6 +4,7 @@ package app.model.doctor;
 import app.util.ApiResponse;
 import app.util.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +33,7 @@ public class DoctorController extends BaseController {
     @GetMapping("/{doctorId}")
     public ResponseEntity<ApiResponse<Doctor>> getDoctorById(@PathVariable("doctorId") Long doctorId){
         Optional<Doctor> doctor = doctorService.getDoctorById(doctorId);
-        return doctor.map(this::ok).orElseGet(() -> this.error(STR."Doktorri me id \{doctorId} nuk ekziston"));
+        return doctor.map(this::ok).orElseGet(() -> this.error(STR."Doktorri me id \{doctorId} nuk ekziston", HttpStatus.NOT_FOUND));
     }
 
 }
