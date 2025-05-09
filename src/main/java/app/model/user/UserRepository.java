c188f4dabf1fa6ec8479ac60007e6b1bfcfb3d56
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-05-09 02:35:34 +0200

    model.User package
    implementimi i plote dhe dokumentimi

--- src/main/java/app/model/user/UserRepository.java
+++ src/main/java/app/model/user/UserRepository.java
@@ -4,10 +4,7 @@ import app.model.authorization.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public Role getRoleById(long id);
+    Role getRoleById(long id);
 }
-
