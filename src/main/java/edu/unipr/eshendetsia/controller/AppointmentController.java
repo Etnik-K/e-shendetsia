8700ef99d5216fb311835ba01f87cef939684557
endrit <endritkastrati8@gmail.com>
2025-05-18 20:17:13 +0200

    Mostly completed files/docstring

--- src/main/java/edu/unipr/eshendetsia/controller/AppointmentController.java
+++ src/main/java/edu/unipr/eshendetsia/controller/AppointmentController.java
@@ -7,21 +7,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Kontrollues qe menaxhon te gjitha kerkesat e lidhura me terminet
+ */
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
     private final AppointmentServiceImplementation appointmentServiceImplementation;
 
+    /**
+     * Konstruktor qe inicializon kontrolluesen e termineve
+     *
+     * @param appointmentServiceImplementation implementimi i sherbimit te termineve
+     */
     @Autowired
     public AppointmentController(AppointmentServiceImplementation appointmentServiceImplementation) {
         this.appointmentServiceImplementation = appointmentServiceImplementation;
     }
 
+    /**
+     * Krijon nje termin te ri
+     *
+     * @param appointment termini qe do te krijohet
+     * @return terminin e krijuar
+     */
     @PostMapping
     public ResponseEntity<Appointment> create(@RequestBody Appointment appointment) {
         return ResponseEntity.ok(appointmentServiceImplementation.save(appointment));
     }
 
+    /**
+     * Anulon nje termin ekzistues
+     *
+     * @param id identifikuesi i terminit
+     * @return pergjigje bosh me status 204
+     */
     @PutMapping("/{id}/cancel")
     public ResponseEntity<Void> cancel(@PathVariable Long id) {
         appointmentServiceImplementation.cancel(id);
--- src/main/java/edu/unipr/eshendetsia/exception/InvalidCredentialsException.java
+++ src/main/java/edu/unipr/eshendetsia/exception/InvalidCredentialsException.java
@@ -1,6 +1,16 @@
 package edu.unipr.eshendetsia.exception;
 
+/**
+ * Perjashtim qe hidhet kur perdoruesi vendos kredenciale te pasakta
+ * gjate procesit te verifikimit.
+ * <p>
+ * Ky perjashtim zgjeron RuntimeException dhe perdoret kryesisht
+ * kur perdoruesi vendos gabim emrin ose fjalekalimin.
+ */
 public class InvalidCredentialsException extends RuntimeException {
+    /**
+     * Konstruktori default i cili vendos mesazhin e gabimit
+     */
     public InvalidCredentialsException() {
         super("Perdoruesi/Fjalekalimi eshte gabim");
     }
