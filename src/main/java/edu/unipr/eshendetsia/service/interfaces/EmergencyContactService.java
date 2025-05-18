da469a5ab8760686d63405fc0cb87ff1b0d1ff53
etnik <etnikkelmendi20@gmail.com>
2025-05-18 14:05:40 +0200

    emergencycontact feature(dto should be completed)

---
+++ src/main/java/edu/unipr/eshendetsia/service/interfaces/EmergencyContactService.java
@@ -0,0 +1,13 @@
+package edu.unipr.eshendetsia.service.interfaces;
+
+import edu.unipr.eshendetsia.model.entity.EmergencyContact;
+
+import java.util.List;
+
+public interface EmergencyContactService {
+    public EmergencyContact save(EmergencyContact contact);
+
+    public List<EmergencyContact> getByUserId(Long userId);
+
+    public void delete(Long id);
+}
