21a97ff4ef03cb19028dba8124f840b7def0fcf6
etnik <etnikkelmendi20@gmail.com>
2025-05-18 14:17:36 +0200

    insurance feature(dto should be completed)

---
+++ src/main/java/edu/unipr/eshendetsia/model/dto/InsuranceDTO.java
@@ -0,0 +1,10 @@
+package edu.unipr.eshendetsia.model.dto;
+
+public class InsuranceDTO {
+    public Long id;
+    public Long userId;
+    public String provider;
+    public String policyNumber;
+    public String coverageDetails;
+    public boolean active;
+}
