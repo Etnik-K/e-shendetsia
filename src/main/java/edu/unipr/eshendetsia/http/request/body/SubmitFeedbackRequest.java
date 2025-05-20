5f07844170fbbe16be03ec1e21f2a21c084f308e
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-05-21 01:38:36 +0200

    rreth feedback contact controller request objects dhe shokeve te tij

---
+++ src/main/java/edu/unipr/eshendetsia/http/request/body/SubmitFeedbackRequest.java
@@ -0,0 +1,13 @@
+package edu.unipr.eshendetsia.http.request.body;
+
+import edu.unipr.eshendetsia.model.entity.Feedback;
+
+import java.time.LocalDateTime;
+
+public record SubmitFeedbackRequest (
+        Long id, Long userId, Long doctorId, String feedback, Integer rating, LocalDateTime issuedAt
+) {
+    public Feedback toFeedback() {
+        return new Feedback(id, userId, doctorId, feedback, rating, issuedAt);
+    }
+}
