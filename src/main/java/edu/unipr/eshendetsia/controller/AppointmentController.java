26d8d0433b85c602cb6b9c4d368a7083240ee542
Euronii <euroni1212@gmail.com>
2025-05-20 20:01:19 +0200

    Rafinimi i Appointmentcontroller

--- src/main/java/edu/unipr/eshendetsia/controller/AppointmentController.java
+++ src/main/java/edu/unipr/eshendetsia/controller/AppointmentController.java
@@ -1,8 +1,15 @@
 package edu.unipr.eshendetsia.controller;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import edu.unipr.eshendetsia.controller.base.BaseController;
+import edu.unipr.eshendetsia.exception.InvalidCredentialsException;
+import edu.unipr.eshendetsia.exception.NotFoundException;
+import edu.unipr.eshendetsia.exception.UnauthorizedException;
+import edu.unipr.eshendetsia.http.response.ApiResponse;
 import edu.unipr.eshendetsia.model.entity.Appointment;
 import edu.unipr.eshendetsia.service.interfaces.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/appointments")
-public class AppointmentController {
+public class AppointmentController extends BaseController {
     private final AppointmentService appointmentService;
 
     /**
@@ -30,9 +37,14 @@ public class AppointmentController {
      * @param appointment termini qe do te krijohet
      * @return terminin e krijuar
      */
-    @PostMapping
-    public ResponseEntity<Appointment> create(@RequestBody Appointment appointment) {
-        return ResponseEntity.ok(appointmentService.save(appointment));
+    @PostMapping("/appointments")
+    public ResponseEntity<ApiResponse<Appointment>> create(@RequestBody Appointment appointment) {
+
+        try{
+            return this.ok(appointmentService.save(appointment));
+        } catch (NotFoundException | InvalidCredentialsException exception) {
+            return this.error("Nuk jeni i autorizuar", HttpStatus.UNAUTHORIZED);
+        }
     }
 
     /**
@@ -42,8 +54,14 @@ public class AppointmentController {
      * @return pergjigje bosh me status 204
      */
     @PutMapping("/{id}/cancel")
-    public ResponseEntity<Void> cancel(@PathVariable Long id) {
-        appointmentService.cancel(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<ApiResponse<String>> cancel(@PathVariable Long id) {
+        try{
+
+            this.appointmentService.cancel(id);
+            return this.ok("Eshte anuluar");
+        } catch (JWTVerificationException | UnauthorizedException exception) {
+            return this.error("Nuk jeni i autorizuar", HttpStatus.UNAUTHORIZED);
+
+        }
     }
 }
