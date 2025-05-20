34c7a78ee7e0fa5fdbecf5fb2451f7c67cd13b1d
Euronii <euroni1212@gmail.com>
2025-05-20 16:30:52 +0200

    rregullimi i dependency per AppointmentController

--- src/main/java/edu/unipr/eshendetsia/controller/AppointmentController.java
+++ src/main/java/edu/unipr/eshendetsia/controller/AppointmentController.java
@@ -1,8 +1,8 @@
 package edu.unipr.eshendetsia.controller;
 
 import edu.unipr.eshendetsia.model.entity.Appointment;
-import edu.unipr.eshendetsia.repository.AppointmentRepository;
 import edu.unipr.eshendetsia.service.implementation.AppointmentServiceImplementation;
+import edu.unipr.eshendetsia.service.interfaces.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,16 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
-    private final AppointmentServiceImplementation appointmentServiceImplementation;
+    private final AppointmentService appointmentService;
 
     /**
      * Konstruktor qe inicializon kontrolluesen e termineve
      *
-     * @param appointmentServiceImplementation implementimi i sherbimit te termineve
+     * @param appointmentService implementimi i sherbimit te termineve
      */
     @Autowired
-    public AppointmentController(AppointmentServiceImplementation appointmentServiceImplementation) {
-        this.appointmentServiceImplementation = appointmentServiceImplementation;
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
     }
 
     /**
@@ -33,7 +33,7 @@ public class AppointmentController {
      */
     @PostMapping
     public ResponseEntity<Appointment> create(@RequestBody Appointment appointment) {
-        return ResponseEntity.ok(appointmentServiceImplementation.save(appointment));
+        return ResponseEntity.ok(appointmentService.save(appointment));
     }
 
     /**
@@ -44,7 +44,7 @@ public class AppointmentController {
      */
     @PutMapping("/{id}/cancel")
     public ResponseEntity<Void> cancel(@PathVariable Long id) {
-        appointmentServiceImplementation.cancel(id);
+        appointmentService.cancel(id);
         return ResponseEntity.noContent().build();
     }
 }
