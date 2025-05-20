ae1749743610fb60dcda2d522d9912a0f5125775
Euronii <euroni1212@gmail.com>
2025-05-20 16:30:52 +0200

    rregullimi i dependency per AppointmentController

--- src/main/java/edu/unipr/eshendetsia/service/interfaces/AppointmentService.java
+++ src/main/java/edu/unipr/eshendetsia/service/interfaces/AppointmentService.java
@@ -1,7 +1,9 @@
 package edu.unipr.eshendetsia.service.interfaces;
 
+import edu.unipr.eshendetsia.model.entity.Appointment;
 import org.springframework.stereotype.Service;
 
 public interface AppointmentService {
-
+    Appointment save(Appointment appointment);
+    void cancel(Long id);
 }
