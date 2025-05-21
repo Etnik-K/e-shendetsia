21a4cbb3e6c3b657c8112b4d3e1639a258e99711
endrit <endritkastrati8@gmail.com>
2025-05-22 01:44:47 +0200

    Integration Tests last touches

--- src/test/java/IntegrationTest/CacheConfingTest.java
+++ src/test/java/IntegrationTest/CacheConfingTest.java
@@ -10,6 +10,9 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.test.context.ContextConfiguration;
 
+/**
+ * Klasa e testimit per konfigurimin e cache.
+ */
 @SpringBootTest
 @ContextConfiguration(classes = CacheConfing.class)
 class CacheConfingTest {
@@ -17,6 +20,13 @@ class CacheConfingTest {
     @Autowired
     private CacheManager cacheManager;
 
+    /**
+     * Teston nese menaxheri i cache eshte konfiguruar si duhet.
+     * Kontrollon nese:
+     * - Menaxheri i cache nuk eshte null
+     * - Menaxheri i cache eshte instance e CaffeineCacheManager
+     * - Cache "history" ekziston ne menaxher
+     */
     @Test
     void testCacheManagerConfiguration() {
         assertNotNull(cacheManager, "Cache manager should not be null");
