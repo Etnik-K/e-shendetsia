2dda6471c5d2790fea2ac40f39caa0f0e29d8eae
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-05-21 02:29:50 +0200

    rreth perscription controller ne request objects dhe shokeve te tij

--- src/main/java/edu/unipr/eshendetsia/controller/PerscriptionController.java
+++ src/main/java/edu/unipr/eshendetsia/controller/PerscriptionController.java
@@ -4,10 +4,14 @@
  */
 package edu.unipr.eshendetsia.controller;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import edu.unipr.eshendetsia.controller.base.BaseController;
+import edu.unipr.eshendetsia.exception.UnauthorizedException;
+import edu.unipr.eshendetsia.http.request.body.CreatePerscriptionRequest;
 import edu.unipr.eshendetsia.http.response.ApiResponse;
 import edu.unipr.eshendetsia.model.entity.Perscription;
 import edu.unipr.eshendetsia.service.interfaces.PerscriptionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,12 +35,16 @@ public class PerscriptionController extends BaseController {
     /**
      * Krijon nje recete te re
      *
-     * @param prescription receta qe do te krijohet
+     * @param prescriptionRequest receta qe do te krijohet
      * @return receta e krijuar dhe ruajtur ne databaze
      */
     @PostMapping
-    public ResponseEntity<ApiResponse<Perscription>> create(@RequestBody Perscription prescription) {
-        Perscription saved = perscriptionService.save(prescription);
-        return this.ok(saved);
+    public ResponseEntity<ApiResponse<String>> create(@RequestBody CreatePerscriptionRequest prescriptionRequest) {
+        try{
+            perscriptionService.save(prescriptionRequest.toPerscription());
+            return this.ok("Receta u krijua me sukses");
+        } catch (UnauthorizedException | JWTVerificationException e){
+            return this.error("Nuk jeni i autorizuar", HttpStatus.UNAUTHORIZED);
+        }
     }
 }
