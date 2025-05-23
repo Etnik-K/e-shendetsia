63971a9a1843a80550a0725bc4f11094c7cb36e2
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2025-05-23 20:29:50 +0200

    user controlleri e murr veten

--- src/main/java/edu/unipr/eshendetsia/controller/concrete/UserController.java
+++ src/main/java/edu/unipr/eshendetsia/controller/concrete/UserController.java
@@ -1,16 +1,10 @@
 package edu.unipr.eshendetsia.controller.concrete;
 
 import edu.unipr.eshendetsia.http.request.body.LoginRequest;
-import edu.unipr.eshendetsia.exception.concrete.InvalidCredentialsException;
-import edu.unipr.eshendetsia.exception.concrete.NotFoundException;
-import edu.unipr.eshendetsia.exception.concrete.UnauthorizedException;
 import edu.unipr.eshendetsia.model.entity.User;
 import edu.unipr.eshendetsia.service.interfaces.UserService;
-import edu.unipr.eshendetsia.http.response.ApiResponse;
 import edu.unipr.eshendetsia.controller.BaseController;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,13 +27,9 @@ public class UserController extends BaseController{
      * @return Userat, ose mesazhi njoftues
      */
     @GetMapping
-    public ResponseEntity<ApiResponse<List<User>>> getAllUsers(@RequestHeader("Authorization") String requestJwt) {
-        try{
-            List<User> users = userService.getAllUsers(requestJwt);
-            return this.ok(users);
-        } catch (JWTVerificationException  | UnauthorizedException exception) {
-            return this.error("Nuk jeni i autorizuar!", HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<List<User>> getAllUsers(@RequestHeader("Authorization") String requestJwt) {
+        List<User> users = userService.getAllUsers(requestJwt);
+        return this.ok(users);
     }
 
     /**
@@ -48,15 +38,9 @@ public class UserController extends BaseController{
      * @return Userin, ose mesazhis njoftues
      */
     @GetMapping("/{userId}")
-    public ResponseEntity<ApiResponse<User>> getUserById(@PathVariable("userId") Long viewUserId, @RequestHeader("Authorization") String requestJwt) {
-        try{
-            User user = userService.getUserById(viewUserId, requestJwt);
-            return this.ok(user);
-        } catch (JWTVerificationException | UnauthorizedException exception) {
-            return this.error("Nuk jeni i autorizuar", HttpStatus.UNAUTHORIZED);
-        } catch (NotFoundException exception) {
-            return this.error("Nuk u gjet useri", HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<User> getUserById(@PathVariable("userId") Long viewUserId, @RequestHeader("Authorization") String requestJwt) {
+        User user = userService.getUserById(viewUserId, requestJwt);
+        return this.ok(user);
     }
 
     /**
@@ -67,13 +51,9 @@ public class UserController extends BaseController{
      * @return - Mesazh konfirmues se a eshte fshire useri, ne baze te
      */
     @DeleteMapping("/{userId}")
-    public ResponseEntity<ApiResponse<String>> deleteUser(@PathVariable("userId") Long deleteUserId, @RequestHeader("Authorization") String requestJwt) {
-        try{
-            this.userService.deleteUser(deleteUserId, requestJwt);
-            return this.ok("Perdoruesi u fshi me sukses");
-        } catch (JWTVerificationException | UnauthorizedException exception) {
-            return this.error("Nuk jeni i autorizuar", HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") Long deleteUserId, @RequestHeader("Authorization") String requestJwt) {
+        this.userService.deleteUser(deleteUserId, requestJwt);
+        return this.ok("Perdoruesi u fshi me sukses");
     }
 
     /**
@@ -82,23 +62,13 @@ public class UserController extends BaseController{
      * @return JWT token te userit, ose njoftim ne rast qe kredencialet jane gabim.
      */
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<String>> login(@RequestBody LoginRequest loginRequest) {
-        try{
-            return this.ok(this.userService.login(loginRequest.id(), loginRequest.password()));
-        } catch (NotFoundException | InvalidCredentialsException exception) {
-            return this.error("Perdoruese/Fjalekalimi i gabuar", HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        return this.ok(this.userService.login(loginRequest.id(), loginRequest.password()));
     }
 
     @GetMapping("{id}/history")
-    public ResponseEntity<ApiResponse<User>> getUserHistory(@PathVariable Long id, @RequestHeader("Authorization") String requestJwt) {
-        try {
-            return this.ok(this.userService.getUserHistory(id, requestJwt));
-        } catch (JWTVerificationException | UnauthorizedException e) {
-            return this.error("Nuk jeni i autorizuar", HttpStatus.UNAUTHORIZED);
-        } catch (NotFoundException exception) {
-            return this.error("Nuk u gjet useri", HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<String> getUserHistory(@PathVariable Long id, @RequestHeader("Authorization") String requestJwt) {
+        return this.ok(this.userService.getUserHistory(id, requestJwt));
     }
 
 }
